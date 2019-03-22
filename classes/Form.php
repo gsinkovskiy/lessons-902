267fec70974d73e794fc8c5de39936c88d9d5882
Hlib Synkovskyi <gleb.sinkovskiy@gmail.com>
2019-03-22 21:08:46 +0200

    lesson 8

---
+++ classes/Form.php
@@ -0,0 +1,39 @@
+<?php
+
+class Form
+{
+
+	/**
+	 * Fields of form
+	 *
+	 * @var array
+	 */
+	private $fields;
+
+	/**
+	 * @var string
+	 */
+	private $method;
+
+	/**
+	 * @var string
+	 */
+	private $action;
+
+	public function __construct(string $method, string $action)
+	{
+		$this->method = $method;
+		$this->action = $action;
+		$this->fields = [];
+	}
+
+	public function render()
+	{
+		$html = sprintf('<form method="%s" action="%s">', $this->method, $this->action);
+		// $html = '<form method="' . $this->method . '" action="' . $this->action . '">';
+		$html .= '</form>';
+
+		return $html;
+	}
+
+}
