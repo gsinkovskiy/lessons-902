2cab6c71a93b5440fbadb918a722325cf9ef56b9
Hlib Synkovskyi <gleb.sinkovskiy@gmail.com>
2019-04-02 21:14:41 +0300

    lesson 10

--- form.php
+++ form.php
@@ -9,13 +9,22 @@ spl_autoload_register(function ($name) {
 	require __DIR__ . '/classes/' . $name . '.php';
 });
 
-$form = new Form('post', 'action.php');
+Registry::getInstance()->setFormElementClass('form-control');
+
+$form = new Form('post', 'form.php');
 $email = new InputFormField('email', 'Email');
+$email->addValidator(new \Form\Validators\EmptyValidator());
+$email->addValidator(new EmailValidator());
 $form->addField($email);
-$form->addField(new InputFormField('firstName', 'Имя'));
+
+$firstName = new InputFormField('firstName', 'Имя');
+$firstName->addValidator(new EmptyValidator());
+$form->addField($firstName);
+
 $form->addField(new PasswordFormField('password', 'Пароль'));
 $form->addField(new PasswordFormField('passwordConfirmation', 'Подтверждение пароля'));
 $form->addField(new FormButton('Зарегистрироваться'));
+$form->processRequest();
 
 ?>
 <!DOCTYPE html>
