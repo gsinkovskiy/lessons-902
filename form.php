cc884cb409221cbe32c096b3a75a6405a48dc5ff
Hlib Synkovskyi <gleb.sinkovskiy@gmail.com>
2019-04-05 19:07:23 +0300

    lesson 10. Показ ошибок

--- form.php
+++ form.php
@@ -1,4 +1,8 @@
 <?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', true);
+
 /* вариант для маленьких проектов
 require_once __DIR__ . '/classes/Form.php';
 require_once __DIR__ . '/classes/FormField.php';
